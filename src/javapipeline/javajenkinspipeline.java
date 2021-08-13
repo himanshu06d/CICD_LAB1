3ec9f832da925eb06be49235d7a3245668cc540e
Himanshu <hd156681@gmail.com>
2021-08-13 16:47:47 +0530

    first commit

---
+++ src/javapipeline/javajenkinspipeline.java
@@ -0,0 +1,10 @@
+package javapipeline;
+
+public class javajenkinspipeline {
+
+	public static void main(String[] args) {
+		System.out.println("This is java via jenkins");
+		System.out.println("This is for seconf commit");
+	}
+
+}
